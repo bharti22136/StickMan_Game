beeda265b138cba113017264a91684328a821e26
Bharti Bansal <131953103+bharti22136@users.noreply.github.com>
2023-12-10 05:54:31 +0530

    Update MainGame.java

--- AP_project 5_3/AP_project 5/AP_project/project1/src/main/java/com/example/project1/MainGame.java
+++ AP_project 5_3/AP_project 5/AP_project/project1/src/main/java/com/example/project1/MainGame.java
@@ -70,7 +70,7 @@ public class MainGame extends StickHero {
 
 //    Pillars pillars=Pillars.getInstance(Rectangle r);
     public MainGame() {
-        super(0, 0, 0); // You may need to provide appropriate default values
+        super(0, 0, 0); 
     }
     public MainGame(double x, double y, double xVelocity) {
         super(x, y, xVelocity);
@@ -113,17 +113,17 @@ public class MainGame extends StickHero {
         backImage1.setImage(backgroundImage);
         backImage2.setImage(backgroundImage);
 
-        // Set up TranslateTransitions for the background images
+        
         TranslateTransition backImageTransition1 = createBackgroundTransition(backImage1);
         TranslateTransition backImageTransition2 = createBackgroundTransition(backImage2);
 
-        // Set up a Timeline to create a loop
+      
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.ZERO, event -> {
-                    // Set the initial positions of the background images
+                   
                     backImage1.setTranslateX(0);
                     backImage2.setTranslateX(backImage1.getBoundsInParent().getWidth());
-                    // Start the TranslateTransitions
+                   
                     backImageTransition1.playFromStart();
                     backImageTransition2.playFromStart();
                 }),
@@ -136,7 +136,7 @@ public class MainGame extends StickHero {
 
         setMusic();
 
-        //Timeline for Incrementing Stick
+        
         increaseStick();
         Duration stickIncrementDuration = Duration.millis(16);
         incrementStickLengthTimeline = new Timeline(new KeyFrame(stickIncrementDuration, event -> {
@@ -165,18 +165,18 @@ public class MainGame extends StickHero {
 
         } else if (event1.getCode() == KeyCode.ENTER && event1.getEventType() == KeyEvent.KEY_RELEASED) {
             isEnterPressed = false;
-            // Stop growStickTimeline when enter is released
+            
             incrementStickLengthTimeline.stop();
-            // Rotate the stick when enter is released
+           
             stickFallDown(stickRectangle,stickMan);
 
         }
         else if (event1.getCode() == KeyCode.SHIFT) {
             if (event1.isShiftDown()) {
-                // Rotate heroGroup by 180 degrees when shift is pressed
+             
                 rotateHeroGroup();
             }
-            // Consume event to prevent unwanted behavior in the application
+            
             event1.consume();
         }
         else{
@@ -195,13 +195,13 @@ public class MainGame extends StickHero {
     }
     private void rotateHeroGroup() {
         if (!isRotated) {
-            // Rotate heroGroup by 180 degrees
+            
             stickMan.setRotate(stickMan.getRotate() + 180);
         } else {
-            // Reverse the rotation by 180 degrees
+           
             stickMan.setRotate(stickMan.getRotate() - 180);
         }
-        // Toggle rotation state
+        
         isRotated = !isRotated;
     }
 
@@ -216,23 +216,22 @@ public class MainGame extends StickHero {
 //        pillarList.add(pillar4);
 //    }
 //    private void checkCollision() {
-//        // Get bounds of stickHero and cherryImage
+//       
 //        Bounds stickHeroBounds = stickHero.getBoundsInParent();
 //        Bounds cherryBounds = cherryImage.getBoundsInParent();
 //
-//        // Check for collision
+//        
 //        if (stickHeroBounds.intersects(cherryBounds)) {
 //            handleCollision();
 //        }
 //    }
 
 //    private void handleCollision() {
-//        // Increment the counter and update the UI or perform any other actions
+//       
 //        cherriesCollected++;
 //        System.out.println("Cherries Collected: " + cherriesCollected);
 //
-//        // You can add more logic here, such as removing the cherryImage, updating the score, etc.
-//        // For example:
+//        
 //        cherriesPane.getChildren().remove(cherryImage);
 //    }
 //    public void switchToScorePage() {
